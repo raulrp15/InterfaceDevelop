93631b4a80de7441ceafdacb4c82fc0bf352f15a
raulrp15 <raulromerapavon@gmail.com>
2024-10-09 17:55:46 +0200

    jiji

--- XAML/Unidad06/Ejercicio01/obj/Debug/net8.0-ios/iossimulator-x64/Ejercicio01.AssemblyInfo.cs
+++ XAML/Unidad06/Ejercicio01/obj/Debug/net8.0-ios/iossimulator-x64/Ejercicio01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fae7e8a46e24a6cf791b6d56f93a0962eeb90166")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a7907d58216d769d175803adbd2b23a3eb161097")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

0bad7b4037b622dd9f09792b90105534c0eeeb5f
raulrp15 <raulromerapavon@gmail.com>
2024-10-21 14:37:51 +0200

    Ejercicio05 realizando

---
+++ Layouts/Unidad08/Ejercicio05ENT/ClsPersona.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio05ENT
+{
+    public class ClsPersona
+    {
+        #region Propiedades
+        public string Nombre { get; set; }
+        public string Apellidos {  get; set; }
+        public int Edad {  get; set; }
+        #endregion
+    }
+}
---
+++ Layouts/Unidad08/Ejercicio05UI/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Ejercicio05DAL;
+using Ejercicio05ENT;
+using System.Collections.ObjectModel;
+
+namespace Ejercicio05UI
+{
+    public partial class MainPage : ContentPage
+    {
+        List<ClsPersona> personas = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            personas = ClsListadoPersonas.getListadoCompletoPersonas();
+
+            lvPersona.ItemsSource = personas;
+        }
+        
+    }
+
+}
